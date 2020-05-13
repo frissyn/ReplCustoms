c7f96b3f759fe5b8b9d5e60e7ae9134ea841eb0b
IreTheKID <62220201+IreTheKID@users.noreply.github.com>
2020-05-13 13:45:14 -0400

    🔥 Changes for v1.1.0 Update
    Added an exception handler for each request to ReplTalk API. Created an error log file to write errors the occur.

--- app.py
+++ app.py
@@ -5,17 +5,43 @@ import repltalk
 from forms import SearchDatabase
 from flask import Flask, render_template, request
 
+def log_error(a, b, c, d):
+	errTemplate = """
+Error Occured - {0}
+
+Type: {1}
+Exception: {2}
+Traceback:
+{3}
+"""
+	with open('Repl-Customs/logs/err_log.txt', 'w') as fh:
+		res = errTemplate.format(a, b, c, d)
+		fh.write(res)
+
 async def get_user_object(name):
 	global user, posts, result, comments
 	replit = repltalk.Client()
+
 	try:
 		user = await replit.get_user(name)
-		posts = await user.get_posts(order='new')
-		comments = await user.get_posts(order='new')
 	except Exception as e:
-		result = str(e)
-		user, posts, comments = None, None, None
-		return
+		user = None
+		ex_type, ex, tb = sys.exc_info()
+		log_error(e, ex_type, ex, tb)
+
+	try:
+		posts = await user.get_posts(limit=5, order='new')
+	except Exception as e:
+		posts = None
+		ex_type, ex, tb = sys.exc_info()
+		log_error(e, ex_type, ex, tb)
+
+	try:
+		comments = await user.get_comments(limit=5, order='new')
+	except Exception as e:
+		comments = None
+		ex_type, ex, tb = sys.exc_info()
+		log_error(e, ex_type, ex, tb)
 
 KEY = os.getenv('APP_KEY')
 
@@ -38,6 +64,7 @@ def application():
 		query = request.form['searchData']
 		key = request.form.get('searchType')
 		if key == '2':
+			str(query).replace('@', '', 1)
 			asyncio.run(get_user_object(str(query)))
 			try:
 				s = user.subscription
@@ -55,9 +82,9 @@ def application():
 					Either the user was not found, or another error has occured.
 					<br><br><br>
 					Error Stack: <br>
-					<b>{str(ex_type)}</b><br>
+					<b>{str(ex_type).replace('<', '').replace('>', '')}</b><br>
 					{str(tb)}<br>
-					{str(ex)}"""
+					{str(ex).replace('<', '').replace('>', '')}"""
 	return render_template('app.html', title='Search', form=form)
 
 if __name__ == '__main__':
